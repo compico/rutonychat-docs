b18fb044912366fee84bcae2a5cbd625b63e0361
compico <compico@mail.ru>
2021-09-07 12:14:49 +0300

    add gulp

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rutonychat-docs
+
 Документация API на C# для RutonyChat
+
+Шаблон HTML взят отсюда:
+https://github.com/harnishdesign/iDocs
---
+++ app/js/app.js
@@ -0,0 +1,5 @@
+// // Import vendor jQuery plugin example
+// import '~/app/libs/mmenu/docs/mmenu.js'
+
+let timer;
+document.addEventListener("DOMContentLoaded", () => {});
